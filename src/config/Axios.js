cc4c612d9c51a0bf6eb901fe2fafdad4eec21077
Deepakgantayat <deepakgantayat29@gmail.com>
2023-07-12 09:37:48 +0530

    completed

--- src/config/Axios.js
+++ src/config/Axios.js
@@ -1,3 +1,4 @@
 import axios from 'axios';
-axios.defaults.baseURL = 'http://localhost:3002'
+axios.defaults.baseURL = 'https://nodedemo-kouy.onrender.com/'
+// 'http://localhost:3002'
 export default axios;
